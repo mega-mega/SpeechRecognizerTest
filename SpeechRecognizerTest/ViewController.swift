b4dbce58960c5abfd58674104d65a3af47f7ca8a
Tshibuya <t.shibuya.uva@gmail.com>
2018-02-20 15:12:39 +0900

    Initial Commit

---
+++ SpeechRecognizerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognizerTest
+//
+//  Created by T Shibuya on 2018/02/20.
+//  Copyright © 2018年 T S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
